18d91fa3ab0b6852a0c2a90ea5015a1ac0872a35
crow <d4v3.oliveira@gmail.com>
2024-07-30 22:29:02 -0300

    bug tipo string no fs

--- index.ts
+++ index.ts
@@ -1,11 +1,22 @@
 import * as fs from "fs";
 
-function lerArquivo(path: string) {
+function lerArquivo() {
     try {
-        const dados = fs.readFileSync(path, "utf-8");
+        const dados = fs.readFileSync("./bd.json", "utf-8");
         console.log(dados);
     } catch (error) {
-        console.log(`Erro: ${error}`);
+        console.log(error);
     }
 }
-lerArquivo("./bd.json");
+
+function escreverArquivo(dados: any) {
+    try {
+        fs.writeFileSync("./bd.json", JSON.stringify(dados));
+        console.log("Arquivo escrito com sucesso!");
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// lerArquivo("./bd.jsons");
+escreverArquivo({ nome: "asdf", teste: 123 });
